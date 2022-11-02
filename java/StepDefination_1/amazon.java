cdcb6c6882863548e4b976614187b12e9bee333a
SnehaDemanna <99233550+SnehaDemanna@users.noreply.github.com>
2022-11-02 19:12:54 +0530

    Add files via upload

---
+++ java/StepDefination_1/amazon.java
@@ -0,0 +1,132 @@
+package StepDefination_1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class amazon {
+
+WebDriver driver;
+
+@Given("Browser is launched")
+public void browser_is_launched() 
+{
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\sneha_demanna\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();	
+}
+
+@And("User navigates to {string} page and check if {string} is present")
+public void user_navigates_to_page_and_check_if_is_present(String url, String ispresent )
+{	
+	driver.get(url);
+	Assert.assertTrue(driver.findElement(By.id(ispresent)).isDisplayed());
+}
+
+@And("type product {string} in {string}")
+public void type_product_in(String product, String element) 
+{
+	driver.findElement(By.id(element)).sendKeys(product);
+}
+
+@And("click on the {string} element and check if {string} element is present")
+public void click_on_the_element_and_check_if_element_is_present(String element, String ispresent) 
+{
+	driver.findElement(By.id(element)).click();
+	String ActualText = driver.findElement(By.xpath(ispresent)).getText();
+	Assert.assertEquals(ActualText ,"RESULTS");
+}
+
+@And("Scroll down")
+public void scroll_down() throws InterruptedException 
+{   
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+	js.executeScript("window.scrollBy(0,800)", "");
+	Thread.sleep(5000);
+	
+}
+
+@Then("click on {string} having desired product")
+public void click_on_having_desired_product(String element) 
+{
+	driver.findElement(By.cssSelector(element)).click();
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	WebDriver driver;
+    @Given("browser is launched")
+    public void browser_is_launched()
+    {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\sneha_demanna\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();	
+    }
+    
+    @And("user navigate to \"(.*)\" page and check if \"(.*)\" is present")
+    public void navigate(String url,String ispresent)
+    {
+		driver.get(url);
+    	Assert.assertTrue(driver.findElement(By.id(ispresent)).isDisplayed());
+    }
+    @And("type the product \"(.*)\" in \"(.*)\"$")
+    public void type_product(String product, String element)
+    {
+    	driver.findElement(By.id(element)).sendKeys(product);
+    }
+    		
+    @And("click on \"(.*)\" element and if \"(.*)\" element is present")
+    public void click_on_search(String element, String ispresent )
+    {
+    	driver.findElement(By.id(element)).click();
+		String ActualText = driver.findElement(By.className(ispresent)).getText();
+		Assert.assertEquals(ActualText ,"RESULTS");
+    }
+    
+    @And("scroll down")
+    public void scroll_down()
+    {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,360)", "");
+    }
+    
+    @Then("click on the \"(.*)\" having desired product")
+    public void click_on_desired(String element)
+    {
+    	driver.findElement(By.cssSelector(element)).click();
+    }
+	
+*/	
+}
